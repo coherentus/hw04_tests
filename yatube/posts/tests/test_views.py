386e39c6f205006e61b7635a8ebb77b8b82a7724
coherentus <coherentus@gmail.com>
2021-05-25 13:57:26 +0300

    end test tmp

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -39,7 +39,7 @@ class URLPathTemplatesTests(TestCase):
         cls.test_post = Post.objects.create(
             author=cls.user_with_post,
             text='test_post_text'
-        )        
+        )
 
     def setUp(self):
         # авторизованный клиент с постом
@@ -47,7 +47,6 @@ class URLPathTemplatesTests(TestCase):
         self.authorized_client_a.force_login(
             URLPathTemplatesTests.user_with_post
         )
-        
 
     def test_right_temlate_use_with_url(self):
         """Проверка, что по запросу url используется верный шаблон"""
@@ -110,29 +109,14 @@ class ViewsContextTests(TestCase):
             group=cls.group_test
         )
 
-    def setUp(self):
-        pass
-
-    def page_queryset_post_test(self, queryset, client, view_name):
-        guest_client = Client()
-        db_post = ViewsContextTests.test_post
-        response = client.get(reverse(view_name))
+    def page_queryset_post_test(self, response):
+        post_in_db = ViewsContextTests.test_post
         # контекст содержит page
         self.assertIn('page', response.context)
 
-        page_list = response.context.get(queryset).object_list
-        first_object_post = page_list[0]
-        self.assertIn(first_object_post, 'text')
-        self.assertIn(first_object_post, 'author')
-        self.assertIn(first_object_post, 'pub_date')
-
-        post_text = first_object_post.text
-        post_author = first_object_post.author
-        post_pub_date = first_object_post.pub_date
-        
-        self.assertEqual(post_text, db_post.text)
-        self.assertEqual(post_author, db_post.author)
-        self.assertEqual(post_pub_date, db_post.pub_date)
+        page_list = response.context.get('page').object_list
+        post_in_context_page = page_list[0]
+        self.assertEqual(post_in_context_page, post_in_db)
 
     # страницы без форм, неавторизованный клиент
     # name "index"
@@ -142,24 +126,8 @@ class ViewsContextTests(TestCase):
         Должно передаваться в шаблон page: QuerySet[Post]
         """
         guest_client = Client()
-        self.page_queryset_post_test('page', guest_client, 'index')
-
-        """response = guest_client.get(reverse('index'))
-        # контекст содержит page
-        self.assertFalse(response.context['page'] is None)
-
-        # page содержит post, post содержит "text" "username" "pub_date"
-        page_list = response.context.get('page').object_list
-        first_object_post = page_list[0]
-        post_text = first_object_post.text
-        post_author = first_object_post.author
-        post_pub_date = first_object_post.pub_date
-        self.assertFalse(post_text is None)
-        self.assertEqual(post_text, ViewsContextTests.test_post.text)
-        self.assertFalse(post_author is None)
-        self.assertEqual(post_author, ViewsContextTests.test_post.author)
-        self.assertFalse(post_pub_date is None)
-        self.assertEqual(post_pub_date, ViewsContextTests.test_post.pub_date)"""
+        response = guest_client.get(reverse('index'))
+        self.page_queryset_post_test(response)
 
     # name "group"
     def test_group_put_in_render_right_context(self):
@@ -174,41 +142,13 @@ class ViewsContextTests(TestCase):
                 kwargs={'slug': ViewsContextTests.group_test.slug}
             )
         )
-
-        # контекст содержит page
-        self.assertFalse(response.context['page'] is None)
-
-        # page содержит post, post содержит "text" "username" "pub_date"
-        page_list = response.context.get('page').object_list
-        first_object_post = page_list[0]
-        post_text = first_object_post.text
-        post_author = first_object_post.author
-        post_pub_date = first_object_post.pub_date
-        self.assertFalse(post_text is None)
-        self.assertEqual(post_text, ViewsContextTests.test_post.text)
-        self.assertFalse(post_author is None)
-        self.assertEqual(post_author, ViewsContextTests.test_post.author)
-        self.assertFalse(post_pub_date is None)
-        self.assertEqual(post_pub_date, ViewsContextTests.test_post.pub_date)
+        self.page_queryset_post_test(response)
 
         # контекст содержит group
-        self.assertFalse(response.context['group'] is None)
-        object_group = response.context['group']
-        group_description = object_group.description
-        group_title = object_group.title
-        group_slug = object_group.slug
-
-        # есть ли group.description, равен ли тестовому полю
-        self.assertFalse(group_description is None)
-        self.assertEqual(group_description,
-                         ViewsContextTests.group_test.description)
-        # есть ли group.title, равен ли тестовому полю
-        self.assertFalse(group_title is None)
-        self.assertEqual(group_title, ViewsContextTests.group_test.title)
-
-        # есть ли group.slug, равен ли тестовому полю
-        self.assertFalse(group_slug is None)
-        self.assertEqual(group_title, ViewsContextTests.group_test.title)
+        group_in_db = ViewsContextTests.group_test
+        self.assertIn('group', response.context)
+        group_in_context = response.context['group']
+        self.assertEqual(group_in_context, group_in_db)
 
     # name "profile"
     def test_profile_put_in_render_right_context(self):
@@ -223,30 +163,13 @@ class ViewsContextTests(TestCase):
                 kwargs={'username': ViewsContextTests.user_test.username}
             )
         )
+        self.page_queryset_post_test(response)
 
         # контекст содержит profile_user
-        self.assertFalse(response.context['profile_user'] is None)
-        object_user = response.context['profile_user']
-        user_username = object_user.username
-
-        # есть ли user.username, равен ли тестовому полю
-        self.assertFalse(user_username is None)
-        self.assertEqual(user_username, ViewsContextTests.user_test.username)
-
-        # контекст содержит page
-        self.assertFalse(response.context['page'] is None)
-
-        # page содержит post, post содержит "text" "username" "pub_date"
-        first_object_post = response.context['page'][0]
-        post_text = first_object_post.text
-        post_author = first_object_post.author
-        post_pub_date = first_object_post.pub_date
-        self.assertFalse(post_text is None)
-        self.assertEqual(post_text, ViewsContextTests.test_post.text)
-        self.assertFalse(post_author is None)
-        self.assertEqual(post_author, ViewsContextTests.test_post.author)
-        self.assertFalse(post_pub_date is None)
-        self.assertEqual(post_pub_date, ViewsContextTests.test_post.pub_date)
+        user_in_db = ViewsContextTests.user_test
+        self.assertIn('profile_user', response.context)
+        user_in_context = response.context['profile_user']
+        self.assertEqual(user_in_context, user_in_db)
 
     # name "group_index"
     def test_group_index_put_in_render_right_context(self):
@@ -258,32 +181,20 @@ class ViewsContextTests(TestCase):
         response = guest_client.get(reverse('group_index'))
 
         # контекст содержит page
-        self.assertFalse(
-            response.context['page'] is None)
+        self.assertIn('page', response.context)
 
         # page содержит group[], group содержит "title" "description" "slug"
-        first_object_group = response.context['page'][0]
-        group_title = first_object_group.title
-        group_description = first_object_group.description
-        group_slug = first_object_group.slug
+        group_in_db = ViewsContextTests.group_test
+        group_in_context = response.context['page'][0]
+        self.assertEqual(group_in_context, group_in_db)
 
-        # group title
-        self.assertFalse(group_title is None)
-        self.assertEqual(group_title, ViewsContextTests.group_test.title)
-        # group description
-        self.assertFalse(group_description is None)
-        self.assertEqual(group_description, ViewsContextTests.group_test.description)
-        # group slug
-        self.assertFalse(group_slug is None)
-        self.assertEqual(group_slug, ViewsContextTests.group_test.slug)
-
-    # name post
+    # name "post"
     def test_post_put_in_render_right_context(self):
         """Проверка, что "post" выдаёт верный контекст в шаблон.
 
         Должно передаваться в шаблон post: Post, author: Post.author
         """
-        guest_client = Client()        
+        guest_client = Client()
         response = guest_client.get(
             reverse(
                 'post',
@@ -294,15 +205,19 @@ class ViewsContextTests(TestCase):
             )
         )
         # контекст содержит post author
-        self.assertFalse(response.context['post'] is None)
-        self.assertFalse(response.context['author'] is None)
+        self.assertIn('post', response.context)
+        self.assertIn('author', response.context)
+        post_in_db = ViewsContextTests.test_post
+        post_in_context = response.context['post']
+        user_in_db = ViewsContextTests.user_test
+        user_in_context = response.context['author']
         # "post" равен тестовому посту
-        self.assertEqual(response.context['post'], ViewsContextTests.test_post)
+        self.assertEqual(post_in_context, post_in_db)
         # "author" из контекста равен тестовому юзеру
-        self.assertEqual(response.context['author'], ViewsContextTests.user_test)
+        self.assertEqual(user_in_context, user_in_db)
 
 
-class YaTb_test_post_route_right_group(TestCase):
+class PostRouteRightGroup(TestCase):
     """Проверка создания поста в группе
 
     После создания поста в группе, он должен:
@@ -310,6 +225,7 @@ class YaTb_test_post_route_right_group(TestCase):
     - появиться на странице своей группы
     - отсутствовать на странице не своей группы
     """
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -317,7 +233,6 @@ class YaTb_test_post_route_right_group(TestCase):
         cls.user_test = User.objects.create(
             username='very_test_user'
         )
-
         # тестовая группа для поста
         cls.group_test_post = Group.objects.create(
             title='test_group_title_with_post',
@@ -331,65 +246,63 @@ class YaTb_test_post_route_right_group(TestCase):
             slug='test-slug-no-post'
         )
 
-        # неавторизованный клиент
-        cls.guest_client = Client()
-
     def setUp(self):
-        self.test_class = YaTb_test_post_route_right_group
-
         # тестовый пост
-        self.test_post = Post.objects.create(
-            author=self.test_class.user_test,
+        PostRouteRightGroup.test_post = Post.objects.create(
+            author=PostRouteRightGroup.user_test,
             text='test_post_text',
-            group=self.test_class.group_test_post
+            group=PostRouteRightGroup.group_test_post
         )
 
     def test_post_after_create_in_index(self):
         """Проверка, что пост попадает на главную"""
-        response = self.test_class.guest_client.get(reverse('index'))
+        guest_client = Client()
+        response = guest_client.get(reverse('index'))
 
-        # post.id с главной и post.id созданного тестового поста
+        # post с главной и post созданный
         self.assertEqual(
-            response.context['page'][0].id,
-            self.test_post.id
+            response.context['page'][0],
+            PostRouteRightGroup.test_post
         )
 
     def test_post_after_create_in_self_group(self):
         """Проверка, что пост попадает в свою группу"""
-        response = self.test_class.guest_client.get(
+        guest_client = Client()
+        response = guest_client.get(
             reverse(
                 'group',
-                kwargs={'slug': self.test_class.group_test_post.slug}
+                kwargs={'slug': PostRouteRightGroup.group_test_post.slug}
             )
         )
 
-        # post.id со страницы руппы и post.id созданного тестового поста
+        # post со страницы руппы и post созданный тестовоый пост
         self.assertEqual(
-            response.context['page'][0].id, self.test_post.id)
+            response.context['page'][0], PostRouteRightGroup.test_post)
 
     def test_post_after_create_not_in_another_group(self):
         """Проверка, что пост не попадает в чужую группу"""
 
         # новый пост для второй группы, чтобы "page" содержал объект
-        self.test_class.test_post_2 = Post.objects.create(
-            author=self.test_class.user_test,
+        PostRouteRightGroup.test_post_2 = Post.objects.create(
+            author=PostRouteRightGroup.user_test,
             text='test_post_text',
-            group=self.test_class.group_test_nopost
+            group=PostRouteRightGroup.group_test_nopost
         )
 
-        response = self.test_class.guest_client.get(
+        guest_client = Client()
+        response = guest_client.get(
             reverse(
                 'group',
-                kwargs={'slug': self.test_class.group_test_nopost.slug}
+                kwargs={'slug': PostRouteRightGroup.group_test_nopost.slug}
             )
         )
 
-        # post.id со страницы группы и post.id созданного тестового поста
+        # post со страницы группы и post созданный тестовоый пост
         self.assertNotEqual(
-            response.context['page'][0].id, self.test_post.id)
+            response.context['page'][0], PostRouteRightGroup.test_post)
 
 
-class YaTb_test_paginator_index_group_profile_groupindex(TestCase):
+class PaginatorWorkRight(TestCase):
     """Проверка пагинатора для всех страниц, где он должен работать
 
     view_name           objects
@@ -408,102 +321,88 @@ class YaTb_test_paginator_index_group_profile_groupindex(TestCase):
         )
 
         # тестовые группы для 'group_index'
-        for i in range(8):
-            Group.objects.create(
-                title='test_group_title_with_post' + str(i),
-                description='test_description_for_test_group_of_post' + str(i),
-                slug='test-slug-for-post' + str(i)
-            )
+        groups_12 = (
+            Group(title='Test %s' % i,
+                  description='test_description%s' % i,
+                  slug='test-slug-%s' % i) for i in range(12)
+        )
+        Group.objects.bulk_create(groups_12)
 
         # тестовые посты для 'index' 'group' 'profile'
-        for i in range(6):
-            Post.objects.create(
-                text='test_text_' + str(i),
-                author=cls.user_test,
-                group=Group.objects.get(id=1)
-            )
-
-        # неавторизованный клиент
-        cls.guest_client = Client()
+        posts_12 = (
+            Post(text='test_text_%s' % i,
+                 author=cls.user_test,
+                 group=Group.objects.get(id=1)) for i in range(12)
+        )
+        Post.objects.bulk_create(posts_12)
 
     def setUp(self):
-        self.test_class = YaTb_test_paginator_index_group_profile_groupindex
-
         # набор для subTest 'index' 'group' 'profile'
         self.test_name_args = {
             'index': '',
             'group': {'slug': Group.objects.get(id=1).slug},
-            'profile': {'username': self.test_class.user_test.username}
+            'profile': {'username': PaginatorWorkRight.user_test.username}
         }
 
     def test_item_posts_per_page(self):
         """Проверка, что все посты правильно разбиваются на страницы.
 
-        Всего постов - 6,
-        константа пагинатора - 4,
-        Ожидаемая разбивка - 4 для первой страницы и 2 для второй(последней).
+        Всего постов - 12,
+        константа пагинатора - 10,
+        Ожидаемая разбивка - 10 для первой страницы и 2 для второй(последней).
         """
-        settings.PAGINATOR_DEFAULT_SIZE = 4
+        guest_client = Client()
         test_array = self.test_name_args
         for name, args in test_array.items():
 
             with self.subTest(name=name):
 
                 # Первая порция(страница)
-                response = self.test_class.guest_client.get(
+                response = guest_client.get(
                     reverse(name, kwargs=args)
                 )
 
                 obj_list = response.context['page']
                 self.assertEqual(
-                    len(obj_list), settings.PAGINATOR_DEFAULT_SIZE,
-                    ('Количество постов на первой странице "', name,
-                     '" не равно константе settings.PAGINATOR_DEFAULT_SIZE.')
+                    len(obj_list), settings.PAGINATOR_DEFAULT_SIZE
                 )
 
                 # Последняя порция(страница)
-                response = self.test_class.guest_client.get(
+                response = guest_client.get(
                     reverse(name, kwargs=args) + '?page=2'
                 )
 
                 obj_list = response.context['page']
                 self.assertEqual(
                     len(obj_list),
-                    Post.objects.count() - settings.PAGINATOR_DEFAULT_SIZE,
-                    ('Количество постов на второй странице "', name,
-                     '" не равно остатку постов.')
+                    Post.objects.count() - settings.PAGINATOR_DEFAULT_SIZE
                 )
 
     def test_item_groups_per_page(self):
         """Проверка, что все группы правильно разбиваются на страницы.
 
-        Всего групп - 8,
-        константа пагинатора - 5,
-        Ожидаемая разбивка - 5 для первой страницы и 3 для второй(последней).
+        Всего групп - 12,
+        константа пагинатора - 10,
+        Ожидаемая разбивка - 10 для первой страницы и 2 для второй(последней).
         """
-        settings.PAGINATOR_DEFAULT_SIZE = 5
+        guest_client = Client()
         # Первая порция(страница)
-        response = self.test_class.guest_client.get(
+        response = guest_client.get(
             reverse('group_index')
         )
 
         obj_list = response.context['page']
         self.assertEqual(
-            len(obj_list), settings.PAGINATOR_DEFAULT_SIZE,
-            ('Количество групп на первой странице "/group" не равно'
-             ' константе settings.PAGINATOR_DEFAULT_SIZE.')
+            len(obj_list), settings.PAGINATOR_DEFAULT_SIZE
         )
 
         # Последняя порция(страница)
-        response = self.test_class.guest_client.get(
+        response = guest_client.get(
             reverse('group_index') + '?page=2'
         )
 
         obj_list = response.context['page']
         self.assertEqual(
             len(obj_list),
-            Group.objects.count() - settings.PAGINATOR_DEFAULT_SIZE,
-            ('Количество групп на второй странице "/group" не равно'
-             ' остатку постов.')
+            Group.objects.count() - settings.PAGINATOR_DEFAULT_SIZE
         )
-
