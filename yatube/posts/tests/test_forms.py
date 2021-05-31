326ea351ba9e79ca5513d9fe40aceffff3383638
coherentus <75427478+coherentus@users.noreply.github.com>
2021-06-01 01:45:46 +0300

    image in form post not load

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,6 +1,5 @@
 import shutil
 import tempfile
-import os
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -52,7 +51,7 @@ class TestCreateEditPostForm(TestCase):
         user_author = self.authorized_author
         small_gif = (
             b'\x47\x49\x46\x38\x39\x61\x01\x00'
-            b'\x01\x00\x00\x00\x00\x21\xf9\x04'
+            b'\x01\x00\x00\x01\x00\x21\xf9\x04'
             b'\x01\x0a\x00\x01\x00\x2c\x00\x00'
             b'\x00\x00\x01\x00\x01\x00\x00\x02'
             b'\x02\x4c\x01\x00\x3b'
@@ -69,7 +68,7 @@ class TestCreateEditPostForm(TestCase):
             'image': uploaded,
         }
         count_post_before = Post.objects.count()
-        
+
         print(form_data)
         print(settings.MEDIA_ROOT)
 
